b917499f0c4a532d49e5de064cb5d05d9c20a20e
Sunjoo IM <tjss1580@gmail.com>
2022-07-23 23:15:08 +0900

    220714

---
+++ 220714/복습_다시해보기/14.py
@@ -0,0 +1,29 @@
+# 문자열 word가 주어질 때, 해당 문자열에서 a 개수를 구하세요.
+# count() 메서드 사용 금지
+
+# case 1.
+word = 'apple'
+cnt = 0
+
+for i in word:
+    if i == 'a':
+        cnt += 1
+print(cnt)
+
+# case 2.
+word = 'banana'
+cnt = 0
+
+for i in word:
+    if i == 'a':
+        cnt += 1
+print(cnt)
+
+# case 3.
+word = 'kiwi'
+cnt = 0
+
+for i in word:
+    if i == 'a':
+        cnt += 1
+print(cnt)
