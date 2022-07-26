f1fef65763d26fd97d440e0f8b14256f7fcc5246
Sunjoo IM <tjss1580@gmail.com>
2022-07-27 01:28:23 +0900

    220719

--- 220719/복습_다시해보기/20.py
+++ 220719/복습_다시해보기/20.py
@@ -1,4 +1,19 @@
 # 정수 number가 주어질 때, 각 자릿수의 합을 구해서 출력하세요.
 
 number = int(input())
+answer = 0
 
+while number:
+    answer += number % 10
+    number //= 10
+print(answer)
+
+# divmod(a, b)
+# a를 b로 나누고, 그 결과를 tuple로 반환해준다. (몫, 나머지)
+number = int(input())
+answer = 0
+
+while number:
+    number, remainder = divmod(number, 10)
+    answer += remainder
+print(answer)
